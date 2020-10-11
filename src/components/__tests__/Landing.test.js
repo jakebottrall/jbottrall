765dfa419952b20fc96c05b846f3ac6225c1b6f8
Jake Bottrall <jakebottrall@gmail.com>
2020-10-11 09:15:43 -0700

    update landing test to describe function

--- src/components/__tests__/Landing.test.js
+++ src/components/__tests__/Landing.test.js
@@ -10,26 +10,34 @@ console.warn = jest.fn(() => "warn");
 
 afterEach(cleanup);
 
-test("<Landing />", () => {
-  const { getByTestId, getAllByTestId } = render(
-    <MemoryRouter>
-      <Landing />
-    </MemoryRouter>
-  );
+describe("<Landing />", () => {
+  it("renders without warnings or errors", () => {
+    const {} = render(
+      <MemoryRouter>
+        <Landing />
+      </MemoryRouter>
+    );
+    expect(console.error).not.toBeCalled();
+    expect(console.warn).not.toBeCalled();
+  });
 
-  //   check title
-  const title = getByTestId("landing-page-title");
-  expect(title.textContent).toBeTruthy();
+  it("renders expected components", () => {
+    const { getByTestId, getAllByTestId } = render(
+      <MemoryRouter>
+        <Landing />
+      </MemoryRouter>
+    );
 
-  //   check subtitle
-  const subtitle = getByTestId("landing-page-subtitle");
-  expect(subtitle.textContent).toBeTruthy();
+    //   check title
+    const title = getByTestId("landing-page-title");
+    expect(title.textContent).toBeTruthy();
 
-  //   check nav items have rendered
-  const navLinks = getAllByTestId("nav-link");
-  expect(navLinks.length).toBe(navItems.length);
+    //   check subtitle
+    const subtitle = getByTestId("landing-page-subtitle");
+    expect(subtitle.textContent).toBeTruthy();
 
-  //   check if any errors or warnings are present
-  expect(console.error).not.toHaveBeenCalled();
-  expect(console.warn).not.toHaveBeenCalled();
+    //   check nav items have rendered
+    const navLinks = getAllByTestId("nav-link");
+    expect(navLinks.length).toBe(navItems.length);
+  });
 });
